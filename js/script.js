c065d7eb643103b85fd47a50e59636d80ad12b2c
Anderson <afs10@cin.ufpe.br>
2018-01-12 11:37:49 -0300

    Terceiro commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*
+alert("Olá HCODE");
+
+var resposta = confirm("Deseja excluir?");
+
+console.log(resposta);
+*/
+
+const a = 10;
+const b = 33;
+
+function somar (a , b) {
+    return a + b;
+}
+
+document.querySelector("#calcular").addEventListener("click", function() {
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+
+    if (valorA.length > 0 && valorB.length > 0) {
+        alert(parseInt(valorA) + parseInt(valorB));
+    } else {
+        alert("Digite os números para soma.");
+    }
+});
